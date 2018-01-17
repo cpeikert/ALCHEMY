383b8cfe45e9ffc9cfd42e63808c2836a4e67d6f
Eric <ecrockett0@gmail.com>
2018-01-16 21:38:22 -0800

    Added files for interactive development

---
+++ AllMain.hs
@@ -0,0 +1,18 @@
+{-|
+Module      : AllMain
+Description : Pulls in all modules.
+Copyright   : (c) Eric Crockett, 2011-2017
+                  Chris Peikert, 2011-2017
+License     : GPL-3
+Maintainer  : ecrockett0@email.com
+Stability   : experimental
+Portability : POSIX
+
+This module depends on all source code modules, so it is useful for checking
+that all code compiles, including all top-level executables.
+-}
+
+import qualified Arithmetic ()
+--import qualified HomomRLWR ()
+--import qualified Tunnel ()
+--import NoiseExperiment ()
