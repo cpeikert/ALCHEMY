bc01af5ba7a16f4e7c633abc07e5c0a1d44cf9ea
Chris Peikert <cpeikert@alum.mit.edu>
2018-08-13 21:39:27 -0400

    fixed up instances and updated to new type-naturals

--- Crypto/Alchemy/Interpreter/PT2CT/Noise.hs
+++ Crypto/Alchemy/Interpreter/PT2CT/Noise.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE ConstraintKinds            #-}
 {-# LANGUAGE DataKinds                  #-}
 {-# LANGUAGE DeriveFunctor              #-}
+{-# LANGUAGE ExplicitNamespaces         #-}
 {-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GADTs                      #-}
@@ -18,9 +19,6 @@
 {-# LANGUAGE TypeOperators              #-}
 {-# LANGUAGE UndecidableInstances       #-}
 
-{-# OPTIONS_GHC -fno-warn-unused-binds  #-}
-
--- internal module, so just export everything
 module Crypto.Alchemy.Interpreter.PT2CT.Noise
 ( PNoise(..), PNZ, PNoiseCyc(..), Units(..)
 , UnitsToNat, ZqPairsWithUnits, TotalUnits, pNoiseUnit
@@ -31,16 +29,16 @@ import           Algebra.Additive             as Additive (C)
 import           Algebra.Ring                 as Ring (C)
 import           Algebra.ZeroTestable         as ZeroTestable (C)
 import           Control.Monad.Random
-import           Data.Functor.Trans.Tagged
-import           Data.Singletons.Prelude      hiding ((:+), (:<))
+import           Data.Singletons.Prelude
 import           Data.Singletons.Prelude.List (Sum)
-import           Data.Singletons.TH           hiding ((:<))
-import           Data.Type.Natural            as N
-import           GHC.TypeLits                 hiding (Nat)
+import           Data.Singletons.TH
+import           Data.Type.Natural
+import           GHC.TypeLits                 hiding (type (+), type (<=),
+                                               Nat)
 import qualified GHC.TypeLits                 as TL (Nat)
 import           Language.Haskell.TH
 
-import Crypto.Lol                      (Factored)
+import Crypto.Lol                      (LiftOf)
 import Crypto.Lol.Cyclotomic.Language
 import Crypto.Lol.Reflects
 import Crypto.Lol.Types.Unsafe.ZqBasic
@@ -55,7 +53,7 @@ type PNZ = 'PN 'Z
 
 -- | Adds a @Nat@ to a @PNoise@.
 type family (:+) a b where
-  'PN a :+ b = 'PN (a N.+ b)
+  'PN a :+ b = 'PN (a + b)
 
 -- | A type representing the number of noise "units" in a modulus.
 -- We use the promoted type @'Units@ of kind @Units@ to distinguish this
@@ -67,31 +65,21 @@ type family UnitsToNat (u :: Units) where
   UnitsToNat ('Units h) = h
 
 -- | A cyclotomic ring element tagged by @pNoise =~ -log(noise rate)@.
-newtype PNoiseCyc (p :: PNoise) c (m :: Factored) r = PNC { unPNC :: c m r }
+newtype PNoiseCyc p c m r = PNC { unPNC :: c m r }
 
-deriving instance (Eq             (c m r)) => Eq             (PNoiseCyc p c m r)
-deriving instance (Show           (c m r)) => Show           (PNoiseCyc p c m r)
-deriving instance (Random         (c m r)) => Random         (PNoiseCyc p c m r)
-deriving instance (Additive.C     (c m r)) => Additive.C     (PNoiseCyc p c m r)
-deriving instance (Ring.C         (c m r)) => Ring.C         (PNoiseCyc p c m r)
-deriving instance (ZeroTestable.C (c m r)) => ZeroTestable.C (PNoiseCyc p c m r)
+deriving instance Eq               (c m r) => Eq               (PNoiseCyc p c m r)
+deriving instance Show             (c m r) => Show             (PNoiseCyc p c m r)
+deriving instance Random           (c m r) => Random           (PNoiseCyc p c m r)
+deriving instance Additive.C       (c m r) => Additive.C       (PNoiseCyc p c m r)
+deriving instance Ring.C           (c m r) => Ring.C           (PNoiseCyc p c m r)
+deriving instance ZeroTestable.C   (c m r) => ZeroTestable.C   (PNoiseCyc p c m r)
+deriving instance Cyclotomic       (c m r) => Cyclotomic       (PNoiseCyc p c m r)
 
--- Crypto.Lol.Cyclotomic.Language instances. Cannot derive these since 'c' is not the last type parameter
-instance Cyclotomic (c m r) => Cyclotomic (PNoiseCyc p c m r) where
-  mulG = PNC . mulG . unPNC
-  divG = fmap PNC . divG . unPNC
-  adviseCRT = PNC . adviseCRT . unPNC
-  advisePow = PNC . advisePow . unPNC
-  adviseDec = PNC . adviseDec . unPNC
+-- Crypto.Lol.Cyclotomic.Language instances. Cannot derive these since
+-- 'c' is not the last type parameter, or because of rnd wrapper
 
 instance GSqNormCyc (c m) r => GSqNormCyc (PNoiseCyc p c m) r where
   gSqNorm = gSqNorm . unPNC
-
-instance GaussianCyc (c m q) => GaussianCyc (PNoiseCyc p c m q) where
-  tweakedGaussian = fmap PNC . tweakedGaussian
-
-instance CosetGaussianCyc (c m zp) => CosetGaussianCyc (PNoiseCyc p c m zp) where
-  cosetGaussian = (fmap PNC .) . (. unPNC) . cosetGaussian
 
 instance ExtensionCyc c r => ExtensionCyc (PNoiseCyc p c) r where
   embed = PNC . embed . unPNC
@@ -99,8 +87,17 @@ instance ExtensionCyc c r => ExtensionCyc (PNoiseCyc p c) r where
   powBasis = fmap (map PNC) powBasis
   coeffsCyc = (map PNC .) . (. unPNC) . coeffsCyc
 
+instance GaussianCyc (c m r) => GaussianCyc (PNoiseCyc p c m r) where
+  tweakedGaussian = fmap PNC . tweakedGaussian
+
+type instance LiftOf (PNoiseCyc p c m zp) = PNoiseCyc p c m (LiftOf zp)
+
+instance (CosetGaussianCyc (c m zp), LiftOf (c m zp) ~ c m (LiftOf zp))
+  => CosetGaussianCyc (PNoiseCyc p c m zp) where
+  cosetGaussian = (fmap PNC .) . (. unPNC) . cosetGaussian
+
 instance CRTSetCyc c r => CRTSetCyc (PNoiseCyc p c) r where
-  crtSet = fmap (map PNC) crtSet
+  crtSet = fmap PNC <$> crtSet
 
 instance RescaleCyc (c m) a b => RescaleCyc (PNoiseCyc p c m) a b where
   rescaleCyc = (PNC .) . (. unPNC) . rescaleCyc
@@ -121,7 +118,7 @@ type family L2P a where
 -- | Maps  'Modulus' over a type list.
 type family MapModulus zqs where
   MapModulus '[] = '[]
-  MapModulus (x ': xs) = (Modulus x) ': (MapModulus xs)
+  MapModulus (x ': xs) = Modulus x ': MapModulus xs
 
 -- | A type-lit 'TL.Nat' with a type-natural 'Nat'.
 data TLNatNat = NN TL.Nat Nat
@@ -132,13 +129,13 @@ type family NatOf a where
 -- | Maps 'NatOf' over a type list.
 type family MapNatOf a where
   MapNatOf '[] = '[]
-  MapNatOf (a ': rest) = (NatOf a) ': MapNatOf rest
+  MapNatOf (a ': rest) = NatOf a ': MapNatOf rest
 
 singletons [d|
-             -- | (Singletons version takes a TypeLit, rather than a 'Nat'.)
-             take :: Nat -> [a] -> [a]
-             take Z _            = []
-             take (S n) (x : xs) = x : take n xs
+             -- | (Singletons version takes a 'TypeLit', rather than a 'Nat'.)
+             takeNat :: Nat -> [a] -> [a]
+             takeNat Z _            = []
+             takeNat (S n) (x : xs) = x : takeNat n xs
 
              -- | Given a list and a threshold h, output the length of
              -- the shortest nonempty prefix whose sum is >= h.
@@ -162,7 +159,7 @@ type ZqPairsWithUnits zqs (h :: Units) = List2Pairs (ZqsWithUnits zqs h)
 -- | For a list of moduli @zqs@, a list representing moduli that have
 -- a total of at least @h@ units.
 type ZqsWithUnits zqs (h :: Units) =
-  ZqsWithUnits' (UnitsToNat h :<= Sum (MapNatOf (MapModulus zqs))) h zqs
+  ZqsWithUnits' (UnitsToNat h <= Sum (MapNatOf (MapModulus zqs))) h zqs
 
 -- | The total noise units among the first of the moduli having at
 -- least @h@ units.
@@ -170,7 +167,7 @@ type TotalUnits zqs (h :: Units) = 'Units (Sum (MapNatOf (MapModulus (ZqsWithUni
 
 
 type family ZqsWithUnits' b (h :: Units) zqs where
-  ZqsWithUnits' 'True ('Units h) zqs = Take (PrefixLen (MapNatOf (MapModulus zqs)) h) zqs
+  ZqsWithUnits' 'True ('Units h) zqs = TakeNat (PrefixLen (MapNatOf (MapModulus zqs)) h) zqs
   -- error case
   ZqsWithUnits' 'False ('Units h) zqs =
     TypeError ('Text "ZqsWithUnits: Modulus needs to support at least " ':<>:
@@ -200,4 +197,4 @@ mkModulus q =
   in conT 'NN `appT` mkTypeLit q `appT` mkTypeNat units
 
 instance (Reflects x i) => Reflects ('NN x y) i where
-  value = retag $ value @x
+  value = value @x
