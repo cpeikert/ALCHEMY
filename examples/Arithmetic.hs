80f46fc1205b40b9e1f767566ff2d40b22b4ae20
Chris Peikert <cpeikert@alum.mit.edu>
2018-08-19 02:53:26 -0400

    stripped down Arithmetic to basics

--- examples/Arithmetic.hs
+++ examples/Arithmetic.hs
@@ -35,41 +35,50 @@ type Zqs = '[ Zq $(mkModulus 268440577)
 
 main :: IO ()
 main = do
+{-
   -- print and size
   putStrLn $ "PT expression: "      ++ pprint addMul
   putStrLn $ "PT expression size: " ++ show (size addMul)
-
+-}
   -- evaluate on random arguments
   pt1 <- getRandom
   pt2 <- getRandom
+{-
   let ptresult = eval addMul pt1 pt2
   putStrLn $ "PT evaluation result: " ++ show ptresult
 
   putStrLn $ "PT expression params:\n" ++ params @(PT2CT M'Map Zqs _ _ _ _) addMul
+-}
 
   evalKeysHints (3.0 :: Double) $ do
     -- compile PT->CT once; interpret multiple times using dup
-    ct <- pt2ct @M'Map @Zqs @TrivGad @Int64 addMul
+    -- was: ct
+    ct1 <- pt2ct @M'Map @Zqs @TrivGad @Int64 addMul
+{-
     let (ct1,tmp)  = dup ct
         (ct2,tmp') = dup tmp
         (ct3,ct4)  = dup tmp'
+-}
 
     -- encrypt the arguments
     arg1 <- encrypt $ unPNC pt1
     arg2 <- encrypt $ unPNC pt2
 
+{-
     -- print and params/size the compiled expression
     putStrLnIO $ "CT expression: " ++ pprint ct2
     putStrLnIO $ "CT expression params:\n" ++ params ct3
     putStrLnIO $ "CT expression size: " ++ show (size ct4)
-
+-}
     -- evaluate with error rates
     ct1' <- readerToAccumulator $ writeErrorRates @Int64 ct1
     let (result, errors) = runWriter $ eval ct1' >>= ($ arg1) >>= ($ arg2)
     putStrLnIO "Error rates: "
     liftIO $ mapM_ print errors
 
+{-
     -- check the decrypted result
     decResult <- fromJust <$> readerToAccumulator (decrypt result)
     putStrLnIO $ "Decrypted evaluation result: " ++ show decResult
     putStrLnIO $ if decResult == unPNC ptresult then "PASS" else "FAIL"
+-}
